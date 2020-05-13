e5c1e36729bd4637bb803b84a252f6749cc48a41
João Mira <joao.mira01@gmail.com>
2020-05-13 21:26:53 +0100

    created aside.php layout

---
+++ views/layouts/aside.php
@@ -0,0 +1,73 @@
+<aside>
+    <div class="sidebar">
+        <div class="about-me display-flex flex-column align-center">
+            <h2>Sobre mim</h2>
+            <div class="about-me-img">
+                <img src="assets/images/20200324212714_646421.png" alt="">
+            </div>
+            <div class="about-text">
+                <p>Fusce id mauris auctor, sollicitudin sit amet, hendrerit risus. Aenean auctor erat. Cras dapibus
+                    dolor commodo.</p>
+            </div>
+        </div>
+
+        <div class="search">
+            <form class="aside-form display-flex flex-column align-center" method="POST" action="home.html">
+                <label class="search-label relative" for="search">Procurar</label>
+                <div class="search-input-row display-flex relative">
+                    <input class="search" id="search" type="text" name="search" autocomplete="off">
+                    <button class="search-btn absolute" type="submit" name="send">
+                        <i class="search-icon las la-search"></i>
+                    </button>
+                </div>
+            </form>
+        </div>
+        <!--end search-->
+
+        <div class="latest-articles-container display-flex flex-column align-center">
+            <h2>Artigos recentes</h2>
+            <div class="latest-articles-mq display-flex flex-column align-center">
+                <div class="latest-articles display-flex align-center relative">
+                    <div class="latest-articles-info display-flex flex-column">
+                        <h3>
+                            <a class="stretched-link" href="article.html">Nome do Artigo</a>
+                        </h3>
+                        <time datetime="2020-04-27">27 Abr, 2020</time>
+                    </div>
+                    <div class="latest-articles-img">
+                        <img src="assets/images/1011-5472x3648.jpg" alt="">
+                    </div>
+                </div>
+                <!--end latest-articles-->
+
+                <div class="latest-articles display-flex align-center relative">
+                    <div class="latest-articles-info display-flex flex-column">
+                        <h3>
+                            <a class="stretched-link" href="article.html">Nome do Artigo</a>
+                        </h3>
+                        <time datetime="2020-04-27">27 Abr, 2020</time>
+                    </div>
+                    <div class="latest-articles-img">
+                        <img src="assets/images/102-4320x3240.jpg" alt="">
+                    </div>
+                </div>
+                <!--end latest-articles-->
+
+                <div class="latest-articles display-flex align-center relative">
+                    <div class="latest-articles-info display-flex flex-column">
+                        <h3>
+                            <a class="stretched-link" href="article.html">Nome do Artigo</a>
+                        </h3>
+                        <time datetime="2020-04-27">27 Abr, 2020</time>
+                    </div>
+                    <div class="latest-articles-img">
+                        <img src="assets/images/1059-7360x4912.jpg" alt="">
+                    </div>
+                </div>
+                <!--end latest-articles-->
+            </div>
+        </div>
+        <!--end latest-articles-container-->
+    </div>
+    <!--end sidebar-->
+</aside>
