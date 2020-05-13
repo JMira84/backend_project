ab63845751aa99476355c1b4ca81686febd45389
João Mira <joao.mira01@gmail.com>
2020-05-13 21:18:28 +0100

    created header layout

---
+++ views/layouts/header.php
@@ -0,0 +1,44 @@
+<header>
+    <div class="container">
+        <div class="header-flex-container display-flex align-center relative">
+            <div class="logo">
+                <h1>
+                    <img src="assets/images/logo/logo2.png" alt="This is the blog's logo">
+                </h1>
+            </div>
+
+            <div class="hamburger">
+                <div class="top"></div>
+                <div class="middle"></div>
+                <div class="bottom"></div>
+            </div>
+            <nav class="navbar display-flex align-center space-evenly">
+                <div class="nav-menu display-flex justify-center">
+                    <ul class="nav-list display-flex flex-no-wrap justify-center">
+                        <li><a class="display-block" href="./">Home</a></li>
+                        <li class="dropdown relative">
+                            <a class="sub-list-trigger display-flex align-center space-between" href="">
+                                Categorias
+                                <i class="sub-menu-arrow header-arrow las la-angle-right"></i>
+                            </a>
+                            <ul class="sub-list absolute">
+                                <li><a class="dropdown-links display-block" href="">Moda</a></li>
+                                <li><a class="dropdown-links display-block" href="">Dicas</a></li>
+                                <li><a class="dropdown-links display-block" href="">Receitas</a></li>
+                                <li><a class="dropdown-links display-block" href="">Mamãs e bebés</a></li>
+                                <li><a class="dropdown-links display-block" href="">Escapadinhas</a></li>
+                            </ul>
+                        </li>
+                        <li><a class="display-block" href="">Contacto</a></li>
+                    </ul>
+                </div>
+                <!--end navbar-->
+                <div class="login-container">
+                    <a class="action-button header-login-button" href="">Login</a>
+                </div>
+            </nav>
+        </div>
+        <!--end header-flex-container-->
+    </div>
+    <!--end container-->
+</header>
