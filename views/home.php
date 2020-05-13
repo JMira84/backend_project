fe8b743c99997036e829112cba8952bbfcb173d7
João Mira <joao.mira01@gmail.com>
2020-05-14 00:01:17 +0100

    minor changes to blockquote style and to home view

--- views/home.php
+++ views/home.php
@@ -29,44 +29,6 @@
                 </div><!--end carousel-elements-->
             </div><!--end carousel-content-->
 
-            <div class="slides carousel-content fade flex-column justify-center align-center relative">
-                <div class="carousel-img">
-                    <img src="assets/images/healthy_food.png" alt="">
-                </div>
-                <div class="carousel-elements absolute display-flex flex-column align-center">
-                    <div class="carousel-category">
-                        <a class="carousel-cat-link relative" href="">Receitas</a>
-                    </div>
-                    <div class="carousel-text">
-                        <h2>
-                            <a href="article.html">Nome do Artigo</a>
-                        </h2>
-                    </div>
-                    <div class="carousel-link">
-                        <a href="article.html">Ler mais</a>
-                    </div>
-                </div><!--end carousel-elements-->
-            </div><!--end carousel-content-->
-
-            <div class="slides carousel-content fade flex-column justify-center align-center relative">
-                <div class="carousel-img">
-                    <img src="assets/images/fashion_kids_slidshow.png" alt="">
-                </div>
-                <div class="carousel-elements absolute display-flex flex-column align-center">
-                    <div class="carousel-category">
-                        <a class="carousel-cat-link relative" href="">Mamãs e Bebés</a>
-                    </div>
-                    <div class="carousel-text">
-                        <h2>
-                            <a href="article.html">Nome do Artigo</a>
-                        </h2>
-                    </div>
-                    <div class="carousel-link">
-                        <a href="article.html">Ler mais</a>
-                    </div>
-                </div><!--end carousel-elements-->
-            </div><!--end carousel-content-->
-
             <div class="next-arrow">
                 <i class="next las la-arrow-right justify-center align-center absolute"></i>
             </div>
@@ -109,96 +71,6 @@
                                 </div>
                             </div>
                         </article>
-
-                        <article class="article-list display-flex flex-column align-center">
-                            <figure>
-                                <a href="article.html">
-                                    <img src="assets/images/102-4320x3240.jpg" alt="">
-                                </a>
-                            </figure>
-                            <div class="article-list-category">
-                                <a class="category-link relative" href="">Receitas</a>
-                            </div>
-                            <div class="article-list-title">
-                                <h2>
-                                    <a class="title-link" href="article.html">Nome do Artigo</a>
-                                </h2>
-                            </div>
-                            <div class="article-list-content">
-                                <p>Brig Nelsons folly ahoy league capstan. Man-of-war prow furl parrel red ensign. Tender bilge water gally swing the lead Davy Jones' Locker. Man-of-war...</p>
-                            </div>
-                            <div class="read-more-container">
-                                <a class="read-more-link" href="article.html">Ler mais</a>
-                            </div>
-                            <div class="date-and-author display-flex space-between">
-                                <div class="article-list-date">
-                                    <time datetime="2020-04-27">27 Abr, 2020</time>
-                                </div>
-                                <div class="article-list-author">
-                                    <p>por <a href="" title="Artigos de Autor">Autor</a></p>
-                                </div>
-                            </div>
-                        </article>
-
-                        <article class="article-list display-flex flex-column align-center">
-                            <figure>
-                                <a href="article.html">
-                                    <img src="assets/images/1059-7360x4912.jpg" alt="">
-                                </a>
-                            </figure>
-                            <div class="article-list-category">
-                                <a class="category-link relative" href="">Moda</a>
-                            </div>
-                            <div class="article-list-title">
-                                <h2>
-                                    <a class="title-link" href="article.html">Nome do Artigo</a>
-                                </h2>
-                            </div>
-                            <div class="article-list-content">
-                                <p>Brig Nelsons folly ahoy league capstan. Man-of-war prow furl parrel red ensign. Tender bilge water gally swing the lead Davy Jones' Locker. Man-of-war...</p>
-                            </div>
-                            <div class="read-more-container">
-                                <a class="read-more-link" href="article.html">Ler mais</a>
-                            </div>
-                            <div class="date-and-author display-flex space-between">
-                                <div class="article-list-date">
-                                    <time datetime="2020-04-27">27 Abr, 2020</time>
-                                </div>
-                                <div class="article-list-author">
-                                    <p>por <a href="" title="Artigos de Autor">Autor</a></p>
-                                </div>
-                            </div>
-                        </article>
-
-                        <article class="article-list display-flex flex-column align-center">
-                            <figure>
-                                <a href="article.html">
-                                    <img src="assets/images/1066-2144x1424.jpg" alt="">
-                                </a>
-                            </figure>
-                            <div class="article-list-category">
-                                <a class="category-link relative" href="">Mamãs e Bebés</a>
-                            </div>
-                            <div class="article-list-title">
-                                <h2>
-                                    <a class="title-link" href="article.html">Nome do Artigo</a>
-                                </h2>
-                            </div>
-                            <div class="article-list-content">
-                                <p>Brig Nelsons folly ahoy league capstan. Man-of-war prow furl parrel red ensign. Tender bilge water gally swing the lead Davy Jones' Locker. Man-of-war...</p>
-                            </div>
-                            <div class="read-more-container">
-                                <a class="read-more-link" href="article.html">Ler mais</a>
-                            </div>
-                            <div class="date-and-author display-flex space-between">
-                                <div class="article-list-date">
-                                    <time datetime="2020-04-27">27 Abr, 2020</time>
-                                </div>
-                                <div class="article-list-author">
-                                    <p>por <a href="" title="Artigos de Autor">Autor</a></p>
-                                </div>
-                            </div>
-                        </article>
                     </div>
                     <!--end article-list-container-->
                     <div class="pagination">
@@ -216,22 +88,14 @@
                 </main>
 
                 <?php require("./layouts/aside.php")?>
-                
+
             </div><!--end main-aside-container-->
         </div><!--end container-->
 
-        <div class="back-to-top">
-            <a class="display-block" href="#">
-                <i class="las la-angle-up"></i>
-            </a>
-        </div>
+        <?php require("./layouts/back_to_top.php")?>
 
-        <div class="brightness-effect absolute display-none"></div>
+        <?php require("./layouts/background.php")?>
 
-        <footer class="display-flex justify-center">
-            <div class="footer-container">
-                <p class="footer-info">Developed by &copy;João Mira, 2020</p>
-            </div>
-        </footer>
+        <?php require("./layouts/footer.php")?>
     </body>
 </html>
