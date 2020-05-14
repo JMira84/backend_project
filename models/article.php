bad3fe6434129f913a6ed843c81764d0b7929bb1
João Mira <joao.mira01@gmail.com>
2020-05-14 17:12:38 +0100

    added category_id to get single article query one article model

--- models/article.php
+++ models/article.php
@@ -43,7 +43,7 @@ class Article extends Base {
 
     public function getSingleArticle($id) {
         $query = $this->db->prepare('
-            SELECT a.article_id, a.title, a.content, a.article_img, a.created_at, u.username, u.profile_img, c.category_name
+            SELECT a.article_id, a.title, a.content, a.article_img, a.created_at, u.username, u.profile_img, c.category_name, c.category_id
             FROM articles a
             INNER JOIN users u USING(user_id)
             INNER JOIN categories c USING(category_id)
