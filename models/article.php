97ba0a9b6aa7d2cb20208e9bbe4a3344007ada44
João Mira <joao.mira01@gmail.com>
2020-05-14 15:18:06 +0100

    changed getList function on article model to create pagination

--- models/article.php
+++ models/article.php
@@ -3,19 +3,39 @@ require("base.php");
 
 class Article extends Base {
     public function getList() {
+        $per_page = 4;
+
+        if(isset($_GET["page"])) {
+            $page = $_GET["page"];
+        } else {
+            $page = 1;
+        }
+
+        $page_1 = ($page * $per_page) - $per_page;
+
         $query = $this->db->prepare('
             SELECT a.article_id, a.title, a.content, a.article_img, a.created_at, u.username, u.profile_img, c.category_name
             FROM articles a
             INNER JOIN users u USING(user_id)
             INNER JOIN categories c USING(category_id)
             ORDER BY created_at DESC
+            LIMIT ' . $page_1 . ', ' . $per_page . '
         ');
 
         $query->execute();
 
         $articles = $query->fetchAll(PDO::FETCH_ASSOC);
 
-        return $articles;
+        $query = $this->db->prepare('
+        ');
+
+        $query->execute();
+
+        $count = $query->rowCount();
+
+        $paginations = ceil($count / $per_page);
+
+        return array($articles, $paginations);
     }
 
     public function getLatestArticles() {
