abb7d20f03338228a069171c751c06d87a868c07
João Mira <joao.mira01@gmail.com>
2020-05-14 01:22:17 +0100

    added latest articles query to controller

--- controllers/home.php
+++ controllers/home.php
@@ -3,5 +3,6 @@ require("models/article.php");
 
 $articleModel = new Article();
 $articles = $articleModel->getList();
+$latestArticles = $articleModel->getLatestArticles();
 
 require("views/home.php");
