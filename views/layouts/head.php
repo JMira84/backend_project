487a07db93f6122552df989a0bf30a496b16200d
João Mira <joao.mira01@gmail.com>
2020-05-14 22:01:01 +0100

    updated js script and css link paths

--- views/layouts/head.php
+++ views/layouts/head.php
@@ -1,3 +1,3 @@
- <link rel="stylesheet" type="text/css" href="../resources/css/style.css">
+ <link rel="stylesheet" type="text/css" href="/resources/css/style.css">
  <link rel="stylesheet" type="text/css" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
- <script type="module" src="../resources/js/app.js" defer></script>
+ <script type="module" src="/resources/js/app.js" defer></script>
