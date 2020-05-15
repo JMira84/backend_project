ea18c1416013f529abdfee14697e7b198ce9524f
João Mira <joao.mira01@gmail.com>
2020-05-15 16:57:10 +0100

    created access controller

---
+++ controllers/access.php
@@ -0,0 +1,40 @@
+<?php
+require("models/category.php");
+
+$categoryModel = new Category();
+$categories = $categoryModel->getList();
+
+if(!isset($url_parts[2])) {
+    header("HTTP/1.1 400 Bad Request");
+    die("Request Inválido");
+}
+
+require("models/user.php");
+
+$userModel = new User();
+
+if($url_parts[2] === "register") {
+    if(isset($_POST["send"])) {
+        $res = $userModel->register($_POST);
+
+        if($res) {
+            header("Location: ../access/login");
+            exit;
+        }
+    }
+
+    require("views/register.php");
+} else if($url_parts[2] === "login") {
+    if(isset($_POST["send"])) {
+        $res = $userModel->login($_POST);
+
+        if($res) {
+            header("Location: ../");
+            exit;
+        }
+
+        $message = "Dados Incorrectos";
+    }
+
+    require("views/login.php");
+}
