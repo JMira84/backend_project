a528caeac6ee0decbd5907b39e19944d49ede293
João Mira <joao.mira01@gmail.com>
2020-05-15 02:19:21 +0100

    created register function

--- models/user.php
+++ models/user.php
@@ -15,4 +15,31 @@ class User extends Base {
 
         return $main_admin;
     }
+    
+    public function register($data) {
+        $data = $this->sanitizer($data);
+
+        if(isset($data["send"]) && $_SESSION["csrf_token"] === $data["csrf_token"]) {
+            if(
+                !empty($data["username"]) &&
+                mb_strlen($data["username"]) > 2 ||
+                mb_strlen($data["username"]) <=120 &&
+                !empty($data["password"]) &&
+                mb_strlen($data["password"]) > 6 ||
+                mb_strlen($data["password"]) <= 1000 &&
+                $data["password"] === $data["rep_password"] &&
+                filter_var($data["email"], FILTER_VALIDATE_EMAIL)
+            ) {
+                $query = $this->db->prepare("");
+    
+                $query->execute();
+    
+                return true;
+            }
+    
+            return false;
+        }
+
+        $_SESSION["csrf_token"] = sha1(mt_rand(10000, 99999) + mt_rand(10000, 99999));
+    }
 }
