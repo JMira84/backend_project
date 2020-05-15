aa5c818022198dd4408cb9fc0d9c14579777e52e
João Mira <joao.mira01@gmail.com>
2020-05-15 01:52:35 +0100

    added rich text editor cdn in head layout

--- views/layouts/head.php
+++ views/layouts/head.php
@@ -1,3 +1,4 @@
  <link rel="stylesheet" type="text/css" href="/resources/css/style.css">
  <link rel="stylesheet" type="text/css" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
+ <script src="https://cdn.tiny.cloud/1/0pfxsae5pg52grxcohxvv1skhahyfx5ccmaxey11v9rixjfs/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
  <script type="module" src="/resources/js/app.js" defer></script>
