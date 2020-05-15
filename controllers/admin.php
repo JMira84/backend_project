045a154b3555b68121589fbeb9dd55ba0827b32a
João Mira <joao.mira01@gmail.com>
2020-05-15 21:05:47 +0100

    added create method on admin controllers

--- controllers/admin.php
+++ controllers/admin.php
@@ -13,4 +13,24 @@ if (isset($_SESSION["user_id"])) {
     $user = $userModel->getLoggedUser();
 }
 
-require("views/admin.php");
+require("models/article.php");
+
+$articleModel = new Article();
+
+if(!isset($url_parts[2])) {
+    require("views/admin.php");
+} else if ($url_parts[2] === "create_article") {
+    if(!isset($_SESSION["user_id"])) {
+        header("Location: ../access/login");
+        exit;
+    }
+
+    if(isset($_POST["send"])) {
+        $post_id = $articleModel->create($_POST);
+        if(!empty($post_id)) {
+            header("Location: " . HOME_PATH . "article/" . $post_id);
+        }
+    }
+
+    require("views/create.php");
+}
