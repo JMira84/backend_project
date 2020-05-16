d8a4581bea673b1ebdb5c2f77ef88a7331c1ef3c
João Mira <joao.mira01@gmail.com>
2020-05-16 23:46:41 +0100

    created uploadImage function to use on different views

--- models/base.php
+++ models/base.php
@@ -12,4 +12,48 @@ class Base {
         }
         return $input;
     }
+
+    public function uploadImage() {
+        $accepts = [
+            "png" => "image/png",
+            "jpeg" => "image/jpeg",
+            "jpg" => "image/jpeg"
+        ];
+
+        if (isset($_FILES["article_img"])) {
+            $finfo = finfo_open(FILEINFO_MIME_TYPE);
+            $detected_type = finfo_file($finfo, $_FILES["article_img"]["tmp_name"]);
+
+            if (
+                $_FILES["article_img"]["error"] === 0 &&
+                in_array($detected_type, $accepts) &&
+                $_FILES["article_img"]["size"] > 0 &&
+                $_FILES["article_img"]["size"] < 20000000
+            ) {
+
+                $file_name = date('YmdHis') . "_" . mt_rand(100000, 999999) . "." . array_search($detected_type, $accepts);
+                move_uploaded_file($_FILES["article_img"]["tmp_name"], "uploads/articles/" . $file_name);
+            } else {
+                die("O upload falhou");
+            }
+        } else if(isset($_FILES["profile_img"])) {
+            $finfo = finfo_open(FILEINFO_MIME_TYPE);
+            $detected_type = finfo_file($finfo, $_FILES["profile_img"]["tmp_name"]);
+
+            if (
+                $_FILES["profile_img"]["error"] === 0 &&
+                in_array($detected_type, $accepts) &&
+                $_FILES["profile_img"]["size"] > 0 &&
+                $_FILES["profile_img"]["size"] < 20000000
+            ) {
+
+                $file_name = date('YmdHis') . "_" . mt_rand(100000, 999999) . "." . array_search($detected_type, $accepts);
+                move_uploaded_file($_FILES["profile_img"]["tmp_name"], "uploads/users/" . $file_name);
+            } else {
+                die("O upload falhou");
+            }
+        }
+
+        return $file_name;
+    }
 }
