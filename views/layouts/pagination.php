49fefaaacbea8ad9a156f805c07a234353e025ee
João Mira <joao.mira01@gmail.com>
2020-05-18 16:38:10 +0100

    finished pagination layout

--- views/layouts/pagination.php
+++ views/layouts/pagination.php
@@ -1,26 +1,50 @@
 <div class="page-container">
     <ul class="pages display-flex align-center space-between">
 <?php
-    if($page >= 2) {
-        echo '
-            <li>
-                <a class="prev-page inline-block" href=' . HOME_PATH . '?page=' . $prev .'>
-                    <span>&larr;</span>
-                    Previous
-                </a>
-            </li>
-        ';
-    }
+    if (isset($url_parts[3])) {
+        if ($page >= 2) {
+            echo '
+                    <li>
+                        <a class="prev-page inline-block" href=' . HOME_PATH . 'browse/category/' . $articles[0]['category_id'] . '/?page=' . $prev . '>
+                            <span>&larr;</span>
+                            Anterior
+                        </a>
+                    </li>
+                ';
+        }
 
-    if($page < $count - 4) {
-        echo '
-            <li>
-                <a class="next-page inline-block" href=' . HOME_PATH . '?page=' . $next . '>
-                    Next
-                    <span>&rarr;</span>
-                </a>
-            </li>
-        ';
+        if ($page < $count - 4) {
+            echo '
+                    <li>
+                        <a class="next-page inline-block" href=' . HOME_PATH . 'browse/category/' . $articles[0]['category_id'] . '/?page=' . $next . '>
+                            Próxima
+                            <span>&rarr;</span>
+                        </a>
+                    </li>
+                ';
+        }
+    } else {
+        if($page >= 2) {
+            echo '
+                <li>
+                    <a class="prev-page inline-block" href=' . HOME_PATH . '?page=' . $prev .'>
+                        <span>&larr;</span>
+                        Anterior
+                    </a>
+                </li>
+            ';
+        }
+    
+        if($page < $count - 4) {
+            echo '
+                <li>
+                    <a class="next-page inline-block" href=' . HOME_PATH . '?page=' . $next . '>
+                        Próxima
+                        <span>&rarr;</span>
+                    </a>
+                </li>
+            ';
+        }
     }
 ?>
     </ul>
