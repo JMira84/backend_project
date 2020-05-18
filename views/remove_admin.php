35a85432a4f9437ba2f002de99a1bf784a694798
João Mira <joao.mira01@gmail.com>
2020-05-18 13:05:22 +0100

    created remove_admin view

---
+++ views/remove_admin.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Remover Administrador</title>
+        <?php require("layouts/head.php")?>
+    </head>
+    <body>
+        <?php require("layouts/header.php")?>
+
+        <div class="container">
+            <main>
+                <div class="admin-flex-container display-flex flex-column justify-center align-center">
+                    <div class="admin-container">
+                        <h2 class="admin-heading">Remover Administrador</h2>
+                        <div class="admin-menu-flex-container display-flex align-center">
+                            <form method="POST" action="<?=$_SERVER["REQUEST_URI"]?>" enctype="multipart/form-data">
+<?php
+    foreach($users as $user) {
+        if(!empty($user["is_admin"])) {
+            echo '
+                                    <div class="remove-admin-field field-container display-flex flex-row space-between">
+                                        <label for="remove_admin' . $user["user_id"] . '">' . $user["username"] . '</label>
+                                        <input id="remove_admin' . $user["user_id"] . '" type="checkbox" name="is_admin" value="0">
+                                        <input type="hidden" name="user_id" value="' . $user["user_id"] . '">
+                                    </div>
+            ';
+        }
+    }
+?>
+                                <div class="field-container submit-container">
+                                    <button class="create-btn" type="submit" name="send">Adicionar</button>
+                                </div>
+                            </form>
+                        </div><!--admin-menu-flex-container-->
+                    </div><!--end admin-container-->
+                </div><!--end admin-flex-container-->
+            </main>
+        </div><!--end cointainer-->
+
+        <?php require("layouts/back_to_top.php")?>
+
+        <?php require("layouts/background.php")?>
+
+        <?php require("layouts/footer.php")?>
+    </body>
+</html>
