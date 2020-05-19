46f14dda8ec3c7b15e5b09228de4de6af38a1a3b
João Mira <joao.mira01@gmail.com>
2020-05-19 23:47:34 +0100

    created view to show a list of articles to edit

---
+++ views/edit_article_list.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Eliminar Artigo</title>
+        <?php require("layouts/head.php")?>
+    </head>
+    <body>
+        <?php require("layouts/header.php")?>
+
+        <div class="container">
+            <main>
+                <div class="admin-flex-container display-flex flex-column justify-center align-center">
+                    <div class="admin-container">
+                        <h2 class="admin-heading">Eliminar Artigo</h2>
+                        <div class="admin-menu-flex-container display-flex flex-column align-center">
+<?php
+    foreach($articles as $article) {
+        echo '
+                            <div class="field-container delete-field display-flex flex-row space-between" data-article_id="' . $article["article_id"] . '">
+                                <span>' . $article["title"] . '</span>
+                                <a href="'. HOME_PATH .'admin/edit_article/article/' . $article["article_id"] . '"><i class="edit-icon las la-edit"></i></a>
+                            </div>
+        ';
+    }
+?>
+                            <?php require("layouts/pagination.php")?>
+                        </div><!--admin-menu-flex-container-->
+                    </div><!--end admin-container-->
+                </div><!--end admin-flex-container-->
+            </main>
+        </div><!--end cointainer-->
+
+        <?php require("layouts/back_to_top.php")?>
+
+        <?php require("layouts/background.php")?>
+
+        <?php require("layouts/footer.php")?>
+    </body>
+</html>
