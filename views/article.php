95920477ec32f9e5b5b3642047cee4654c99fe68
João Mira <joao.mira01@gmail.com>
2020-05-20 17:20:24 +0100

    added comment area on article view to show users comments

--- views/article.php
+++ views/article.php
@@ -26,7 +26,7 @@
                                 <time datetime="' . date("j M Y, H:i", strtotime($article["created_at"])) . '">' . strftime('%e %B, %Y', strtotime($article["created_at"])) . '</time>
                             </div>
                             <figure>
-                                <img src="../assets/images/' . $article["article_img"] . '" alt="">
+                                <img src="/uploads/articles/' . $article["article_img"] . '" alt="">
                             </figure>
                             <div class="article-content">
                                 ' . $article["content"] . '
@@ -34,11 +34,13 @@
     
     ';
 ?>
-                            
+<?php
+    if(isset($article["about"]) && isset($article["profile_img"])) {
+?>
                             <section class="author-container">
                                 <div class="author-box display-flex flex-column align-center">
                                     <div class="author-photo">
-                                        <img src="../assets/images/<?php echo $article["profile_img"];?>" alt="">
+                                        <img src="/uploads/users/<?php echo $article["profile_img"];?>" alt="">
                                     </div>
                                     <div class="author-description">
                                         <h3 class="author-title">
@@ -50,77 +52,78 @@
                                     </div><!--end author-description-->
                                 </div><!--end author-box-->
                             </section><!--author-container-->
-
+<?php
+    }
+?>
                             <section class="comments-container">
                                 <h3 class="comments-title relative">Comentários</h3>
                                 <div class="comments-list-container">
                                     <ul class="comments-list">
+<?php
+    foreach($comments as $comment) {
+        echo '
                                         <li>
                                             <div class="comment-body display-flex flex-no-wrap">
                                                 <div class="author-avatar display-flex justify-center">
-                                                    <img src="../assets/images/20200325023752_160057.png" alt="">
+                                                    <img src="/uploads/users/' . $comment["profile_img"] . '" alt="">
                                                 </div>
                                                 <div class="comment-content">
                                                     <div class="author-and-date display-flex align-center flex-no-wrap space-between">
                                                         <div class="comment-author">
-                                                            <h3>Nome do autor</h3>
+                                                            <h3>' . $comment["username"] . '</h3>
                                                         </div>
                                                         <div class="comment-date-and-reply display-flex align-center relative">
-                                                            <time datetime="2020-04-27">27 Abr, 2020</time>
+                                                            <time datetime="' . date("j M Y, H:i", strtotime($comment["created_at"])) . '">' . strftime('%e %B, %Y', strtotime($comment["created_at"])) . '</time>
                                                             <a class="comment-reply-link" href="#comments-form">
                                                                 Responder
                                                             </a>
                                                         </div>
                                                     </div><!--end author-and-date-->
                                                     <div class="text">
-                                                        <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit.</p>
+                                                        <p>' . $comment["comment_content"] . '.</p>
                                                     </div>
                                                 </div><!--end comment-text-->
                                             </div><!--end comment-body-->
-
-                                            <ul class="comments-children">
-                                                <li>
-                                                    <div class="comment-body display-flex flex-no-wrap">
-                                                        <div class="author-avatar display-flex justify-center">
-                                                            <img src="../assets/images/20200329191908_271776.png" alt="">
-                                                        </div>
-                                                        <div class="comment-content">
-                                                            <div class="author-and-date display-flex align-center flex-no-wrap space-between">
-                                                                <div class="comment-author">
-                                                                    <h3>Nome do autor</h3>
-                                                                </div>
-                                                                <div class="comment-date-and-reply display-flex align-center relative">
-                                                                    <time datetime="2020-04-27">27 Abr, 2020</time>
-                                                                    <a class="comment-reply-link" href="#comments-form">
-                                                                        Responder
-                                                                    </a>
-                                                                </div>
-                                                            </div>
-                                                            <!--end author-and-date-->
-                                                            <div class="text">
-                                                                <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit.</p>
-                                                            </div>
-                                                        </div>
-                                                        <!--end comment-text-->
-                                                    </div>
-                                                    <!--end comment-body-->
-                                                </li>
-                                            </ul><!--end comments-children-->
                                         </li>
+        ';
+    }
+?>
                                     </ul>
                                 </div><!--end comments-list-->
 
+<?php
+    if(isset($_SESSION["user_id"])) {
+?>
                                 <div class="comment-response">
-                                    <form id="comments-form" method="POST" action="article.html">
+                                    <form id="comments-form" method="POST" action="<?=$_SERVER["REQUEST_URI"]?>">
                                         <div class="field-container display-flex flex-column">
                                             <label for="comment-area">Deixe um comentário</label>
-                                            <textarea class="comment-area" id="comment-area" name="comment" maxlength="65535" cols="58" rows="6"></textarea>
+<?php
+    if(isset($comments_message)) {
+        echo'
+                                            <div class="alert-message-container display-flex justify-center" role="alert">
+                                                <div class="alert-message display-flex align-center">
+                                                    <span>' . $comments_message . '</span>
+                                                </div>
+                                            </div>
+        ';
+    }
+?>
+                                            <textarea class="comment-area" id="comment-area" name="comment_content" maxlength="65535" cols="58" rows="6"></textarea>
                                         </div>
                                         <div class="inline-block">
+                                            <input type="hidden" name="article_id" value="<?=$article["article_id"]?>">
                                             <button class="action-button" type="submit" name="send">Comentar</button>
                                         </div>
                                     </form>
-                                </div>
+                                </div><!--end comment-response-->
+<?php
+    } else {
+?>
+                                <p>Para escrever um comentário, faça <a class="to-login" href="<?=HOME_PATH?>access/login">login</a>.</p>
+<?php
+    }
+?>
                             </section><!--end comments-container-->
                         </article>
                     </div><!--end article-and-comments-container-->
