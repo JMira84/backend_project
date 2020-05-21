bf7f4e507f29279ffc3893a43477ae83c3dbc760
João Mira <joao.mira01@gmail.com>
2020-05-21 20:54:19 +0100

    edited viewComments query function to support pagination

--- models/comments.php
+++ models/comments.php
@@ -9,25 +9,53 @@ class Comment extends Base {
             VALUES(?, ?, ?)
         ");
 
-        $query->execute([
+        $result = $query->execute([
             $data["article_id"],
             $data["comment_content"],
             $_SESSION["user_id"]
         ]);
+
+        return $result;
     }
 
     public function viewComments() {
-        $query = $this->db->prepare("
+        $page = 0;
+        $per_page = 5;
+        $page_counter = 1;
+        $next = $page_counter + 1;
+        $prev = $page_counter - 1;
+
+        if (isset($_GET["page"])) {
+            $page = (int) $_GET["page"] - 1;
+            $page_counter = (int) $_GET["page"];
+            $page = $page * $per_page;
+            $next = $page_counter + 1;
+            $prev = $page_counter - 1;
+        }
+
+        $query = $this->db->prepare('
             SELECT c.comment_content, c.created_at, u.username, u.profile_img
             FROM comments c
             INNER JOIN users u USING(user_id)
             ORDER BY c.created_at DESC
-        ");
+            LIMIT ' . $page . ', ' . $per_page . '
+        ');
 
         $query->execute();
 
         $comments = $query->fetchAll(PDO::FETCH_ASSOC);
 
-        return $comments;
+        $query = $this->db->prepare("
+            SELECT comment_id, article_id, comment_content, created_at, user_id
+            FROM comments
+        ");
+
+        $query->execute();
+
+        $count = $query->rowCount();
+
+        $paginations = ceil($count / $per_page);
+
+        return array($page, $page_counter, $next, $prev, $comments, $count, $paginations);
     }
 }
