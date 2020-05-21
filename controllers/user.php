dff154b2a5ccbc685b43e4cebdf0bb6539fed914
João Mira <joao.mira01@gmail.com>
2020-05-21 20:19:05 +0100

    created user controller and to include update profile and update password query functions

---
+++ controllers/user.php
@@ -0,0 +1,34 @@
+<?php
+require("models/category.php");
+
+$categoryModel = new Category();
+$categories = $categoryModel->getList();
+
+if (!isset($url_parts[2])) {
+    header("HTTP/1.1 400 Bad Request");
+    die("Request Inválido");
+}
+
+require("models/user.php");
+
+$userModel = new User();
+
+if ($url_parts[2] === "edit_profile") {
+    if (isset($_POST["send_new_info"])) {
+        $res = $userModel->updateInfo($_POST);
+
+        if ($res) {
+            header("Location: ../");
+            exit;
+        }
+    } else if (isset($_POST["send_new_password"])) {
+        $res = $userModel->updatePassword($_POST);
+
+        if ($res) {
+            header("Location: ../");
+            exit;
+        }
+    }
+
+    require("views/edit_profile.php");
+}
