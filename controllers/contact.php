ae3cb652d50018cb645c5dd555f7c66e313905eb
João Mira <joao.mira01@gmail.com>
2020-05-22 00:14:43 +0100

    changed email username and email password to variables stored on config file

--- controllers/contact.php
+++ controllers/contact.php
@@ -1,55 +1,70 @@
 <?php
+require("./vendor/autoload.php");
+require("config.php");
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 
+require("models/category.php");
+
+$categoryModel = new Category();
+$categories = $categoryModel->getList();
+
 require("models/user.php");
 
-$userModel = new User();
-
-if (isset($_SESSION["user_id"])) {
-    $user = $userModel->getLoggedUser();
-}
-
 if(isset($_POST["send"])) {
+    foreach ($_POST as $key => $value) {
+        $_POST[$key] = strip_tags(trim($value));
+    }
 
-    require("/Users/joaom/vendor/autoload.php");
-
-    $mail = new PHPMailer;
-
-    $mail->CharSet = 'UTF-8';
-
-    $mail->isSMTP();
-
-    // $mail->SMTPDebug = SMTP::DEBUG_SERVER;
-
-    $mail->SMTPDebug = 0;
-
-    $mail->Host = "smtp.gmail.com";
-
-    $mail->Port = 587;
-
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-
-    $mail->SMTPAuth = true;
-
-    $mail->Username = "joao.mira01@gmail.com";
-
-    $mail->Password = "kpfdvhxszspmccpk";
-
-    $mail->setFrom($_POST["email"], $_POST["name"]);
-
-    $mail->addAddress("joao.mira01@gmail.com", "João Mira");
-
-    $mail->Subject = $_POST["subject"];
-
-    $mail->Body = $_POST["message"];
-
-    $mail->send();
-
-    if (!$mail->send()) {
-        echo 'Erro de envio: ' . $mail->ErrorInfo;
+    if(
+        !empty($_POST["name"]) &&
+        (mb_strlen($_POST["name"]) >= 2 ||
+        mb_strlen($_POST["name"]) < 120) &&
+        !empty($_POST["subject"]) &&
+        (mb_strlen($_POST["subject"]) > 3 ||
+        mb_strlen($_POST["subject"]) < 120) &&
+        !empty($_POST["message"]) &&
+        filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)
+    ) {
+    
+        $mail = new PHPMailer;
+    
+        $mail->CharSet = 'UTF-8';
+    
+        $mail->isSMTP();
+    
+        // $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+    
+        $mail->SMTPDebug = 0;
+    
+        $mail->Host = "smtp.gmail.com";
+    
+        $mail->Port = 587;
+    
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    
+        $mail->SMTPAuth = true;
+    
+        $mail->Username = $email_address;
+    
+        $mail->Password = $email_password;
+    
+        $mail->setFrom($_POST["email"], $_POST["name"]);
+    
+        $mail->addAddress($email_address, $name);
+    
+        $mail->Subject = $_POST["subject"];
+    
+        $mail->Body = $_POST["message"];
+    
+        if (!$mail->send()) {
+            echo 'Erro de envio: ' . $mail->ErrorInfo;
+        } else {
+            $email_message = "Mensagem enviada";
+        }
     } else {
-        $email_message = "Mensagem enviada";
+        $email_alert_message = "Por favor, preencha a área de texto.";
     }
 }
 
