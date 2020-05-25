2fd41bbc8e7fe19434adc0666bfb5ac41b313128
João Mira <joao.mira01@gmail.com>
2020-05-25 21:21:46 +0100

    called getByDate and getByAuthor functions from article model

--- controllers/browse.php
+++ controllers/browse.php
@@ -15,16 +15,48 @@ if($url_parts[2] === "category") {
     }
     
     if(is_numeric($url_parts[3])) {
-        $articles = $articleModel->getByCategory($url_parts[3]);
+        list($page, $page_counter, $next, $prev, $articles, $count, $paginations) = $articleModel->getByCategory($url_parts[3]);
     }
 
     if(empty($articles)) {
         header("HTTP/1.1 404 Not Found");
         die("Categoria inexistente");
     }
+
+} else if($url_parts[2] === "date") {
+    if (empty($url_parts[3])) {
+        header("HTTP/1.1 400 Bad Request");
+        die("Request Inválido");
+    }
+
+    if (is_numeric($url_parts[3]) && is_numeric($url_parts[4])) {
+        list($page, $page_counter, $next, $prev, $articles, $count, $paginations) = $articleModel->getByDate($url_parts[3], $url_parts[4]);
+    }
+
+    if (empty($articles)) {
+        header("HTTP/1.1 404 Not Found");
+        die("Categoria inexistente");
+    }
+
+} else if($url_parts[2] === "author") {
+    if (empty($url_parts[3])) {
+        header("HTTP/1.1 400 Bad Request");
+        die("Request Inválido");
+    }
+
+    if (is_numeric($url_parts[3])) {
+        list($page, $page_counter, $next, $prev, $articles, $count, $paginations) = $articleModel->getByAuthor($url_parts[3]);
+    }
+
+    if (empty($articles)) {
+        header("HTTP/1.1 404 Not Found");
+        die("Categoria inexistente");
+    }
 }
 
 $latestArticles = $articleModel->getLatestArticles();
+
+$datesList = $articleModel->dateList();
 
 require("models/user.php");
 
