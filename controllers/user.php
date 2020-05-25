6101bde9d0943adcb9b18974a7fd9a75bab65abd
João Mira <joao.mira01@gmail.com>
2020-05-25 21:23:37 +0100

    called updateIngo and updatePassword functions from user model

--- controllers/user.php
+++ controllers/user.php
@@ -13,22 +13,27 @@ require("models/user.php");
 
 $userModel = new User();
 
-if ($url_parts[2] === "edit_profile") {
-    if (isset($_POST["send_new_info"])) {
-        $res = $userModel->updateInfo($_POST);
-
-        if ($res) {
-            header("Location: ../");
-            exit;
+if(isset($_SESSION["user_id"])) {
+    if ($url_parts[2] === "edit_profile") {
+        if (isset($_POST["send_new_info"])) {
+            $res = $userModel->updateInfo($_POST);
+    
+            if ($res) {
+                header("Location: /");
+                exit;
+            }
+        } else if (isset($_POST["send_new_password"])) {
+            $res = $userModel->updatePassword($_POST);
+    
+            if ($res) {
+                header("Location: /");
+                exit;
+            }
         }
-    } else if (isset($_POST["send_new_password"])) {
-        $res = $userModel->updatePassword($_POST);
-
-        if ($res) {
-            header("Location: ../");
-            exit;
-        }
+    
+        require("views/edit_profile.php");
     }
-
-    require("views/edit_profile.php");
+} else {
+    header("Location: /access/login");
+    exit;
 }
